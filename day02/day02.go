484062081884cf41f683e14b3db6653a3e82f5e6
akuyu <78714307+wdmvv@users.noreply.github.com>
2024-12-02 10:58:17 +0100

    not working go day2p2

--- day02/day02.go
+++ day02/day02.go
@@ -29,12 +29,35 @@ func partOne() {
 
 	fmt.Println(sum)
 }
+
+func partTwo() {
+	f, err := os.Open("input.txt")
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	buf := bufio.NewReader(f)
+	sum := 0
+	for {
+		line, _, err := buf.ReadLine()
+		if err != nil {
+			break
+		}
+		ls := strings.Split(string(line), " ")
+		if processTwo(ls) <= 1 {
+			sum += 1
+		}
+	}
+	fmt.Println(sum)
+}
+
 func abs(n int) int {
 	if n < 0 {
 		return -n
 	}
 	return n
 }
+
 func sig(n int) int {
 	if n == 0 {
 		return 0
@@ -61,6 +84,26 @@ func processOne(ls []string) bool {
 	return true
 }
 
+func processTwo(ls []string) int {
+	ln := make([]int, len(ls), len(ls))
+	out := 0
+	for i, j := range ls {
+		ln[i], _ = strconv.Atoi(j)
+	}
+	sigd := sig(ln[1] - ln[0])
+	for i := 1; i < len(ln); i++ {
+		dif := ln[i] - ln[i-1]
+		if abs(dif) == 0 || abs(dif) > 3 {
+			out += 1
+		} else if sig(dif) != sigd {
+			out += 1
+		}
+
+	}
+	return out
+}
+
 func main() {
 	partOne()
+	partTwo()
 }
